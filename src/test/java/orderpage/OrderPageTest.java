fbd85b5e7f3ca92d9985e94b0898f468848df476
Nikolay Shcherbakov <nikchemnii@yandex.ru>
2024-05-14 23:53:05 +0300

    delete unused imports

--- src/test/java/orderpage/OrderPageTest.java
+++ src/test/java/orderpage/OrderPageTest.java
@@ -9,7 +9,6 @@ import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import pom.MainPage;
 import pom.order.ConfirmationModal;
 import pom.order.ContactDetailsPage;
